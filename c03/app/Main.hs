f37d0b464583afc10d5b9cbc003c8fb56d35f861
fieldchester <37942267+fieldchester@users.noreply.github.com>
2020-04-04 19:50:15 +0200

    Update Main.hs

--- c03/app/Main.hs
+++ c03/app/Main.hs
@@ -1,1 +1,44 @@
+module Main where
 
+-- import Lib -- Linken braucht zu lange
+
+
+-- Frage 1
+-- -------
+-- Sieht man oft, mit return am Ende
+res = [0,1,2,3] >>= ( \x -> 
+          [0,1] >>= (\y ->
+ return $ x + y)  )
+
+-- Wieso nicht mit flip fmap am Ende ?
+res1 = [0,1,2,3] >>= ( \x -> 
+ flip fmap [0,1]  (\y ->
+          x  +y)  ) -- schneller, da kein return
+
+-- Frage 2
+-- -------
+-- Functor für IO () : Er ist nicht vorhanden, gibts einen Grund
+-- ausser dass er vielleicht nicht oft verwendet wird
+-- a = fmap reverse $ putStrLn "Hello"
+-- compiler error: exprected Tyep [a] found ()
+
+
+-- Fragen - Ende
+-- -------------
+-- -------------
+
+
+
+main :: IO ()
+main = someFunc -- HIE functioniert nur, wenn mindestens einmal compiliert
+
+someFunc :: IO ()
+someFunc = putStrLn "Hello, type in your name"  >> 
+           getLine                              >>= \name -> 
+           putStrLn $ "Hey" ++ name
+
+
+-- flip fmap mit ()
+res2 = [0,1,2,3] >>= ( \x -> 
+ flip fmap [(),()]  (\y ->
+              x)  ) -- ok
